dd437b2f7c0f86984c81c02425982223838e9374
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-03-25 19:38:44 -0300

    :tada:

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Transactions } from "@/pages/transactions";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
+			<Transactions />
+		</ThemeProvider>
+	);
+}
