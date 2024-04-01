b60985845b0fb22b3197aada789e40a9a559acb2
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-04-01 18:16:39 -0300

    add styles

--- src/components/newTransactionModal/index.tsx
+++ src/components/newTransactionModal/index.tsx
@@ -1,6 +1,6 @@
+import { ArrowCircleDown, ArrowCircleUp, X } from "@phosphor-icons/react";
 import * as Dialog from "@radix-ui/react-dialog";
 import * as S from "./styles";
-import { X } from "@phosphor-icons/react";
 
 export function NewTransactionModal() {
 	return (
@@ -9,13 +9,25 @@ export function NewTransactionModal() {
 			<S.Content>
 				<Dialog.Title>Nova transação</Dialog.Title>
 				<S.Close>
-          <X size={24} />
-        </S.Close>
+					<X size={24} />
+				</S.Close>
 
 				<form action="">
 					<input type="text" placeholder="Descrição" required />
 					<input type="text" placeholder="Preço" required />
 					<input type="text" placeholder="Categoria" required />
+
+					<S.TransactionType>
+						<S.TransactionTypeButton value="income" variant="income">
+							<ArrowCircleUp size={24} />
+							Entrada
+						</S.TransactionTypeButton>
+
+						<S.TransactionTypeButton value="outcome" variant="outcome">
+							<ArrowCircleDown size={24} />
+							Saída
+						</S.TransactionTypeButton>
+					</S.TransactionType>
 
 					<button type="submit">CRIAR</button>
 				</form>
